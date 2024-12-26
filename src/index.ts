783c37ca79d8f0bda83f6696ca88f20fd58eabca
alexanderElsikora <alexander.frontend@elsikora.com>
2024-12-26 20:41:41 +0700

    feat(ui,button): Implement a Button ui comp

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Slot } from '@radix-ui/react-slot';
+export { Button, type ButtonProps } from './shared/Button/Button';
 
 import tailwindConfig from '../tailwind.config';
 export { tailwindConfig };
